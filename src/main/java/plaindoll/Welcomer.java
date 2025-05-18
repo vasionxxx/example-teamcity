7b618a9c8d69a830a9e34c1d41c0755fcb977c14
vasionxxx <144676961+vasionxxx@users.noreply.github.com>
2025-05-18 09:57:04 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,16 +1,24 @@
 package plaindoll;
 
-public class Welcomer{
+public class Welcomer {
 	public String sayWelcome() {
 		return "Welcome home, good hunter. What is it your desire?";
 	}
+
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
-	public String sayNeedGold(){
+
+	public String sayNeedGold() {
 		return "Not enough gold";
 	}
-	public String saySome(){
+
+	public String saySome() {
 		return "something in the way";
+	}
+
+	// Новый метод, содержащий слово "hunter"
+	public String sayHunterPraise() {
+		return "Ahh, brave hunter, your strength is beyond compare!";
 	}
 }
